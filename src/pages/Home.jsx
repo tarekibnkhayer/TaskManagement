47c1055429e6e2bdac6f757a0ddd359b1c57b291
tarekibnkhayer <tarekibnkhayer@gmail.com>
2024-01-20 10:19:31 +0600

    Tailwind CSS and Daisy UI integrated

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-4xl text-center font-bold">Hello From Home</h1>
+        </div>
+    );
+};
+
+export default Home;
